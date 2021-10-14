b05e52c497f104d7e1aff30086b5e94df9d21c8b
Juan Quintero <juanquintero@lahaus.com>
2021-10-14 09:37:17 -0500

    Add web component config

---
+++ src/components/Welcome.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <h1>Welcome to your first Web Component</h1>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Welcome",
+  props: {
+    msg: String,
+  },
+};
+</script>
+<style scoped>
+.container {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
